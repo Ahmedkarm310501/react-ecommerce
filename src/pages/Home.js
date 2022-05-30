7b231c36b6ad211f8c8b1a28eb5940e25cfbdb8e
Abdalla5355 <abdallaashraf534@gmail.com>
2022-05-30 04:03:08 +0200

    change in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Ads from "../components/layout/Ads";
-import Slider from "../components/layout/Est quis pariatur do in ex consequat incididunt fugiat esse ullamco commodo eu. Tempor sunt Lorem dolor nostrud sint in et cupidatat. Nostrud labore incididunt occaecat culpa pariatur laboris irure elit cillum nostrud et excepteur magna. Et anim aliqua elit quis qui.";
+import Slider from "../components/layout/Slider";
 import Product from "./Product";
 import Video1 from "../components/pics/ads/realme GT - Official Product Video.mp4";
 import Video2 from "../components/pics/ads/The New Latitude 9000 Series (2020).mp4";
